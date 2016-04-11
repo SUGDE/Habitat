63e03072725328f024983621d925defe5320963a
Maksym Ponomarenko . netzkern <nk.makpon88@kerngruppe.net>
2016-04-11 18:05:15 +0200

    Habitat settings

---
+++ gulp-config.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    var config = {
+        websiteRoot: "D:\\Projects\\UserGroup\\Website",
+        sitecoreLibraries: "D:\\Projects\\UserGroup\\Website\\bin",
+    solutionName: "Habitat",
+    buildConfiguration: "Debug"
+    }
+    return config;
+}
