752cad978adf3872ce5d5aecc794229e6ea63cc7
maxim88 <nk.makpon88@kerngruppe.net>
2016-04-12 15:20:18 +0200

    Added Sections Rendering

---
+++ src/Project/UserGroup/code/netzkern.UserGroup/Templates.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Data;
+
+namespace netzkern.UserGroup
+{
+  public static class Templates
+  {
+    public static class SectionSublyoutProperties
+    {
+      public static class Fields
+      {
+        public static readonly ID Section_ID = new ID("{117D9486-C1FA-447A-8AD8-8F32311646C8}");
+        public const string Section_ID_FieldName = "Section ID";
+      }
+    }
+  }
+}
