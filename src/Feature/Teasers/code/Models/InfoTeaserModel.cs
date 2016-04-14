11ea03f6abbb7178c0bc34b36dd6e698991f851b
maxim88 <nk.makpon88@kerngruppe.net>
2016-04-14 14:03:02 +0200

    InfoTeaser

---
+++ src/Feature/Teasers/code/Models/InfoTeaserModel.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sitecore.Feature.Teasers.Models
+{
+  public class InfoTeaserModel
+  {
+    public InfoTeaserModel(Item item)
+    {
+      this.Item = item;
+      this.Title = item[Templates.InfoTeaser.Fields.Title];
+      this.Text = item[Templates.InfoTeaser.Fields.Text];
+       
+     var list = (MultilistField)item.Fields[Templates.InfoTeaser.Fields.InfoItems];
+      var infoItems = list.GetItems();
+      this.InfoItems = infoItems.Select(i=>new IconTeaserModel(i)).ToList();
+    }
+
+    public InfoTeaserModel()
+    {
+
+    }
+
+    public Item Item { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public List<IconTeaserModel> InfoItems { get; set; }
+  }
+}
