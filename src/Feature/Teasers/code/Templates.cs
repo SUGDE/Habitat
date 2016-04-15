7fad1ddc2174fa6d320833621f5727b6da858040
maxim88 <nk.makpon88@kerngruppe.net>
2016-04-16 01:34:29 +0200

    Create Agenda teaser

--- src/Feature/Teasers/code/Templates.cs
+++ src/Feature/Teasers/code/Templates.cs
@@ -42,7 +42,32 @@
         public static readonly ID Link = new ID("{E8AB122C-6F54-4D4E-AEC6-F81ADDC558FC}");
       }
     }
-    
+     
+    public struct AccordionElementTeaser
+    {
+      public static ID ID = new ID("{49073CB7-D9A4-426C-8581-8FD84E522B97}");
+
+      public struct Fields
+      {
+        public static readonly ID Time = new ID("{5F084F71-A92C-4512-9E76-FECD94E528F8}");
+        public const string Time_FieldName = "Time";
+
+        public static readonly ID Presenter = new ID("{54B9CF7E-7958-4915-A640-05CE804CF3E4}");
+        public const string Presenter_FieldName = "Presenter";
+
+        public static readonly ID Duration = new ID("{9441D365-53B0-4DAF-A8E3-D6A7F1C79229}");
+        public const string Duration_FieldName = "Duration";
+
+        public static readonly ID PresentersTitle = new ID("{0C670E0D-0CAE-4DBD-8DE9-F4DC210464C8}");
+        public const string PresentersTitle_FieldName = "Presenters Title";
+
+        public static readonly ID Title = new ID("{4A59D072-5B41-4A79-A157-2B2CCAC10F2B}");
+        public static readonly ID Summary = new ID("{13D97A52-7C4E-407C-960D-FADDE8A3C1B1}");
+        public static readonly ID Image = new ID("{0F6B5546-E0AB-4487-81DE-640C1AA1B65B}");
+        public static readonly ID Link = new ID("{E8AB122C-6F54-4D4E-AEC6-F81ADDC558FC}");
+      }
+    }
+
     public struct InfoTeaser
     {
       public static ID ID = new ID("{6B4D1DD5-9D04-4BFC-8780-518E77E6A7D3}");
