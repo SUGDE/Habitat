e9930850fab7c55f600bbffaca51fdfe6dac4238
maxim88 <nk.makpon88@kerngruppe.net>
2016-05-24 15:54:23 +0200

    Created pages Kontakt and Impressum

--- src/Feature/PageContent/code/Models/UserGroup/HighlightItem.cs
+++ src/Feature/PageContent/code/Models/UserGroup/HighlightItem.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Web;
 using Sitecore.Data.Items;
 using Sitecore.Data.Fields;
+using Sitecore.Web.UI.WebControls;
 
 namespace Sitecore.Feature.PageContent.Models.UserGroup
 {
@@ -9,19 +11,22 @@ namespace Sitecore.Feature.PageContent.Models.UserGroup
     public HighlightItem(Item item)
     {
       this.Item = item;
-      this.Title = item[Templates.Highlight.Fields.Title];
-      this.Text = item[Templates.Highlight.Fields.Text];
-      DateField dateField = (DateField)item.Fields[Templates.Highlight.Fields.Date_Countdown];
+      this.Title = new HtmlString(FieldRenderer.Render(item, Templates.Highlight.Fields.Title.ToString()));
+      this.Text = new HtmlString(FieldRenderer.Render(item, Templates.Highlight.Fields.Text.ToString()));
+
+      DateCountdown = new HtmlString(FieldRenderer.Render(item, Templates.Highlight.Fields.Date_Countdown.ToString()));
+       
+      DateCountdownStr = ((DateField)item.Fields[Templates.Highlight.Fields.Date_Countdown]).DateTime.ToString("MM dd yyyy HH:mm:ss");
 
       // DateTime object
-      this.DateCountdown = dateField.DateTime;
-      this.DateCountdownStr = dateField.DateTime.ToString("MM dd yyyy HH:mm:ss");
+      //this.DateCountdown = dateField.DateTime;
+      //this.DateCountdownStr = dateField.DateTime.ToString("MM dd yyyy HH:mm:ss");
     }
 
     public Item Item { get; set; }
-    public string Title { get; set; }
-    public string Text { get; set; }
-    public DateTime DateCountdown { get; set; }
-    public string DateCountdownStr { get; set; }
+    public HtmlString Title { get; set; }
+    public HtmlString Text { get; set; }
+    public HtmlString DateCountdown { get; set; }
+    public string DateCountdownStr { get; set; } 
   }
 }
